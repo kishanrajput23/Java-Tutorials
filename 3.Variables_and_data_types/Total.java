91e9f2d724dc91e3613eb7f7734c52811748e1ed
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-19 16:49:39 +0530

    Variables & Data Types

---
+++ 3.Variables_and_data_types/Total.java
@@ -0,0 +1,10 @@
+public class Total {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=63;
+      int sum=x+y+z;
+
+      System.out.println("Sum of x+y+z = " + sum);
+    }
+}
