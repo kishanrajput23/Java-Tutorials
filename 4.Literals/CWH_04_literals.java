de4557b9cc869ee216e91d5e1e135f4c95b97c05
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-19 17:02:35 +0530

    Create CWH_04_literals.java

---
+++ 4.Literals/CWH_04_literals.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+        
+        System.out.print(age);
+        
+        String str = "Harry";
+        
+        System.out.println(str);
+    }
+}
