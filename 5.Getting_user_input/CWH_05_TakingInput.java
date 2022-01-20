791c4f9eca983d0cb28d0f7766b78dba39be16c5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-20 16:28:52 +0530

    Getting User Input

---
+++ 5.Getting_user_input/CWH_05_TakingInput.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class CWH_05_TakingInpu {
+    public static void main(String[] args) {
+        System.out.println("Taking Input From the User");
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+//        float a = sc.nextFloat();
+//        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+//        float b = sc.nextFloat();
+
+//        int sum = a +b;
+//        float sum = a +b;
+//        System.out.println("The sum of these numbers is");
+//        System.out.println(sum);
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}
