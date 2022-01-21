696180e5bd4d103249c3c270187e9d78ba21a6f1
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-21 19:12:33 +0530

    Operators & Expressions

---
+++ 8.Operators_and_expressions/CWH_Ch2_Operators.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CWH_Ch2_Operators {
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators
+        int a = 4;
+        // int b = 6 % a; // Modulo Operator
+        // 4.8%1.1 --> Returns Decimal Remainder
+
+        // 2. Assignment Operators
+        int b = 9;
+        b *= 3;
+        System.out.println(b);
+
+        // 3. Comparison Operators
+        // System.out.println(64<6);
+
+        // 4. Logical Operators
+        // System.out.println(64>5 && 64>98);
+        System.out.println(64>5 || 64>98);
+
+        // 5. Bitwise Operators
+        System.out.println(2&3);
+        //        10
+        //        11
+        //        ----
+        //        10
+    }
+}
