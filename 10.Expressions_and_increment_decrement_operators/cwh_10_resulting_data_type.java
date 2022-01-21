8f5c859ac3c7a2c49b494110c12055b4a985d68e
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-21 19:46:05 +0530

    Expressions & Increment/Decrement Operators

---
+++ 10.Expressions_and_increment_decrement_operators/cwh_10_resulting_data_type.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class cwh_10_resulting_data_type {
+    public static void main(String[] args) {
+        /* byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b); */
+
+        // Increment and Decrement Operators
+        int i = 56;
+        // int b = i++; // first b is assigned i (56) then i is incremented
+        int j = 67;
+        int c = ++j; // first j is incremented then c is assigned j (68)
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        int y = 7;
+        System.out.println( ++y *8);
+        char ch = 'a';
+        System.out.println(++ch);
+    }
+}
