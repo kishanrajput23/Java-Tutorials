c3d87b111167885d61df966a83e70fb86dc7691a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-24 20:56:15 +0530

    Introduction to Strings

---
+++ 13.Introduction_to_strings/cwh_13_strings.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class cwh_13_strings {
+    public static void main(String[] args) {
+        // String name = new String("Harry");
+        // String name = "Harry";
+        // System.out.print("The name is: ");
+        // System.out.print(name);
+        int a = 6;
+        float b = 5.6454f;
+        System.out.printf("The value of a is %d and value of b is %8.2f", a, b);
+        //System.out.format("The value of a is %d and value of b is %f", a, b);
+        Scanner sc = new Scanner(System.in);
+//        String st = sc.next();
+//        String st = sc.nextLine();
+//        System.out.println(st);
+
+    }
+}
