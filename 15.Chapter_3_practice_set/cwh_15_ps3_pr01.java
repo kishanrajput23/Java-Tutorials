2b65cb9de7e522d8d55e88a04af8d0512ce4e103
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-25 20:53:32 +0530

    Chapter 3 Practice Set Solutions

---
+++ 15.Chapter_3_practice_set/cwh_15_ps3_pr01.java
@@ -0,0 +1,9 @@
+public class cwh_15_ps3_pr01 {
+    public static void main(String[] args) {
+        String name = "Peter Parker";
+        System.out.println(name);
+        
+        name = name.toLowerCase();
+        System.out.println(name);
+    }
+}
---
+++ 15.Chapter_3_practice_set/cwh_15_ps3_pr02.java
@@ -0,0 +1,9 @@
+public class cwh_15_ps3_pr02 {
+    public static void main(String[] args) {
+        String name = "My name is            Kishan       ";
+        System.out.println(name);
+        
+        name = name.replace(" ", "_");
+        System.out.println(name);
+    }
+}
