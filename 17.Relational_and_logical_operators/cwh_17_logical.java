77e8606c727849ddf6f75b39431a59d93d9fe24f
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-26 17:54:41 +0530

    Relational and Logical Operators

---
+++ 17.Relational_and_logical_operators/cwh_17_logical.java
@@ -0,0 +1,28 @@
+public class cwh_17_logical {
+    public static void main(String[] args) {
+        System.out.println("For Logical AND...");
+        boolean a = true;
+        boolean b = false;
+//        if (a && b){
+//            System.out.println("Y");
+//        }
+//        else{
+//            System.out.println("N");
+//        }
+
+        System.out.println("For Logical OR...");
+
+//        if (a || b){
+//            System.out.println("Y");
+//        }
+//        else{
+//            System.out.println("N");
+//        }
+
+        System.out.println("For Logical NOT");
+        System.out.print("Not(a) is ");
+        System.out.println(!a);
+        System.out.print("Not(b) is ");
+        System.out.println(!b);
+    }
+}
