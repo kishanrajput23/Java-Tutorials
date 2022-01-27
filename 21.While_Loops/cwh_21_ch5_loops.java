43b7b97b3a880b4b1ce51c54bf9b71e6806906bd
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-27 16:39:18 +0530

    While Loops

---
+++ 21.While_Loops/cwh_21_ch5_loops.java
@@ -0,0 +1,19 @@
+public class cwh_21_ch5_loops {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        System.out.println("Using Loops:");
+        int i = 100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running While Loop!");
+
+//        while(true){
+//            System.out.println("I am an infinite while loop!");
+//        }
+    }
+}
