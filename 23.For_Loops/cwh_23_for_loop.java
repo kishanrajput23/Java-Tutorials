4dee4c76a5d3e79dbf5dadc7ff05dee0033be848
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-27 17:33:33 +0530

    For Loop

---
+++ 23.For_Loops/cwh_23_for_loop.java
@@ -0,0 +1,17 @@
+public class cwh_23_for_loop {
+    public static void main(String[] args) {
+//        for (int i=1; i<=10; i++){
+//            System.out.println(i);
+//        }
+        // 2i = Even Numbers = 0, 2, 4, 6, 8
+        // 2i+1 = Odd Numbers = 1, 3, 5, 7, 9
+        //int n = 3;
+        //for (int i =0; i<n; i++){
+        //    System.out.println(2*i+1);
+        //}
+
+        for(int i=5; i!=0; i--){
+            System.out.println(i);
+        }
+    }
+}
