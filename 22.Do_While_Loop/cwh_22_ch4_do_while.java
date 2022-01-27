c350e8171d9010daf3af6e88294f1ef5212a5ca0
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-27 17:06:28 +0530

    Do-While Loop

---
+++ 22.Do_While_Loop/cwh_22_ch4_do_while.java
@@ -0,0 +1,21 @@
+public class cwh_22_ch4_do_while {
+    public static void main(String[] args) {
+//        int a = 0;
+//        while(a<5){
+//            System.out.println(a);
+//            a++;
+//        }
+        int b = 10;
+        do {
+            System.out.println(b);
+            b++;
+        }while(b<5);
+
+        int c = 1;
+        do{
+            System.out.println(c);
+            c++;
+        }while(c<=45);
+
+    }
+}
