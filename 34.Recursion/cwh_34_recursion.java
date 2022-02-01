e979503669c1f180aaaa086447970b0c3169bda0
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-01 20:13:02 +0530

    Recursion

---
+++ 34.Recursion/cwh_34_recursion.java
@@ -0,0 +1,32 @@
+public class cwh_34_recursion {
+    // factorial(0) = 1
+    // factorial(n) = n * n-1 *....1
+    // factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+    // factorial(n) = n * factorial(n-1)
+
+    static int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n * factorial(n-1);
+        }
+    }
+    static int factorial_iterative(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            int product = 1;
+            for (int i=1;i<=n;i++){ // 1 to n
+                product *= i;
+            }
+            return product;
+        }
+    }
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println("The value of factorial x is: " + factorial(x));
+        System.out.println("The value of factorial x is: " + factorial_iterative(x));
+    }
+}
