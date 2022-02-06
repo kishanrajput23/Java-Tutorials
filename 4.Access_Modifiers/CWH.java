f1a2f71f40b760108a1af20017ff1cba8514b045
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-06 15:32:25 +0530

    Access modifiers, getters & setters

---
+++ 4.Access_Modifiers/CWH.java
@@ -0,0 +1,15 @@
+class Employee {
+
+    private int id;
+    private  String name;
+
+}
+
+public  class CWH {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.id = 3;
+        emp1.name = "Shubham";
+
+    }
+}
---
+++ 4.Access_Modifiers/cwh_40_ch9.java
@@ -0,0 +1,28 @@
+class MyEmployee{
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public void setId(int i){
+        this.id = i;
+    }
+    public int getId(){
+        return id;
+    }
+}
+public class cwh_40_ch9 {
+    public static void main(String[] args) {
+        MyEmployee harry = new MyEmployee();
+        // harry.id = 45;
+        // harry.name = "CodeWithHarry"; --> Throws an error due to private access modifier
+        harry.setName("CodeWithHarry");
+        System.out.println(harry.getName());
+        harry.setId(234);
+        System.out.println(harry.getId());
+    }
+}
