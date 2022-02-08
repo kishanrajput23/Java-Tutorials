c2c94956ca70063f8818621669a880445735f17b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-08 19:58:26 +0530

    Inheritance in Java

---
+++ 45.Inheritance/cwh_45_inheritance.java
@@ -0,0 +1,42 @@
+class Base{
+    public int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        System.out.println("I am in base and setting x now");
+        this.x = x;
+    }
+
+    public void printMe(){
+        System.out.println("I am a constructor");
+    }
+}
+
+class Derived extends Base{
+    public int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+public class cwh_45_inheritance {
+    public static void main(String[] args) {
+        // Creating an Object of base class
+        Base b = new Base();
+        b.setX(4);
+        System.out.println(b.getX());
+
+        // Creating an object of derived class
+        Derived d = new Derived();
+        d.setY(43);
+        System.out.println(d.getY());
+    }
+}
