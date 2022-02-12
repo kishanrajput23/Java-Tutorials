a6f6bd0a09fc9d2f0badf8a1d897625439684494
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-12 18:34:53 +0530

    Constructors in Inheritance

---
+++ 46.Constructors_In_Inheritance/cwh_46_constructors_in_inheritance.java
@@ -0,0 +1,38 @@
+class Base1{
+    Base1(){
+        System.out.println("I am a constructor");
+    }
+    Base1(int x){
+        System.out.println("I am an overloaded constructor with value of x as: " + x);
+    }
+}
+
+class Derived1 extends Base1{
+    Derived1(){
+        //super(0);
+        System.out.println("I am a derived class constructor");
+    }
+    Derived1(int x, int y){
+        super(x);
+        System.out.println("I am an overloaded constructor of Derived with value of y as: " + y);
+    }
+}
+
+class ChildOfDerived extends  Derived1{
+    ChildOfDerived(){
+        System.out.println("I am a child of derived constructor");
+    }
+    ChildOfDerived(int x, int y, int z){
+        super(x, y);
+        System.out.println("I am an overloaded constructor of Derived with value of z as: " + z);
+    }
+}
+public class cwh_46_constructors_in_inheritance {
+    public static void main(String[] args) {
+        // Base1 b = new Base1();
+        // Derived1 d = new Derived1();
+        // Derived1 d = new Derived1(14, 9);
+        // ChildOfDerived cd = new ChildOfDerived();
+        ChildOfDerived cd = new ChildOfDerived(12, 13, 15);
+    }
+}
