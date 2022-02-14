04d61c710c88bc2a2acd6327a58a95c5c6121a78
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-14 21:31:06 +0530

    this and super keyword in Java

---
+++ 47.this_and_super_keyword/cwh.java
@@ -0,0 +1,20 @@
+class cwh{
+    int x;
+
+//    getter of x
+    public  int getX(){
+        return  x;
+    }
+
+    // Constructor with a parameter
+    cwh(int x) {
+        x = x;
+    }
+
+    // Call the constructor
+    public static void main(String[] args) {
+        cwh obj1 = new cwh(65);
+        System.out.println(obj1.getX());
+
+    }
+}
